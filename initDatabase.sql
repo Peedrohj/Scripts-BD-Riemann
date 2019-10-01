dad177c695244a012396bea641ee9a3a527a0496
OtacilioN <osmn@cesar.org.br>
2019-10-01 19:43:04 -0300

    Prettify scripts

--- initDatabase.sql
+++ initDatabase.sql
@@ -1,7 +1,8 @@
 -- drop database Riemann
 -- Create project database
 DROP DATABASE IF EXISTS Riemann;
-CREATE DATABASE IF NOT EXISTS Riemann;
+CREATE DATABASE
+IF NOT EXISTS Riemann;
 USE Riemann;
 
 -- Drop tables
@@ -18,68 +19,76 @@ DROP TABLE IF EXISTS Faculdade;
 DROP TABLE IF EXISTS Loja_Fisica;
 
 -- Create tables
-CREATE TABLE Aplicativo (
+CREATE TABLE Aplicativo
+(
 	nome VARCHAR(60) NOT NULL,
 	categoria VARCHAR(45) NULL,
 	faz_transacao BIT NULL DEFAULT 0,
 	PRIMARY KEY (nome)
 );
 
-CREATE TABLE Endereco(
-	id_endereco VARCHAR(60) NOT NULL, 
+CREATE TABLE Endereco
+(
+	id_endereco VARCHAR(60) NOT NULL,
 	numero INT(5) NULL,
-    logradouro VARCHAR(90) NULL,
-    cep VARCHAR(8) NULL,
-    cidade VARCHAR(95) NULL,
-    estado VARCHAR(90) NULL,
-    bairro VARCHAR(90) NULL,
-    PRIMARY KEY (id_endereco)
+	logradouro VARCHAR(90) NULL,
+	cep VARCHAR(8) NULL,
+	cidade VARCHAR(95) NULL,
+	estado VARCHAR(90) NULL,
+	bairro VARCHAR(90) NULL,
+	PRIMARY KEY (id_endereco)
 );
 
-CREATE TABLE Pessoa(
+CREATE TABLE Pessoa
+(
 	cpf VARCHAR(11) NOT NULL,
 	nome VARCHAR(90) NULL,
 	data_de_nascimento VARCHAR(10) NULL,
-	id_endereco VARCHAR(60) NULL, 
+	id_endereco VARCHAR(60) NULL,
 	CONSTRAINT fk_endereco_pessoa FOREIGN KEY (id_endereco) REFERENCES Endereco (id_endereco),
 	genero VARCHAR(15) NULL,
 	nome_mae VARCHAR(90) NULL,
 	PRIMARY KEY (cpf)
 );
 
-CREATE TABLE Telefone(
+CREATE TABLE Telefone
+(
 	numero VARCHAR(13) NULL,
-    cpf VARCHAR(11) NOT NULL, 
+	cpf VARCHAR(11) NOT NULL,
 	CONSTRAINT fk_pessoa_faculdade FOREIGN KEY (cpf) REFERENCES Pessoa (cpf)
 );
 
-CREATE TABLE Cartao_Credito(
+CREATE TABLE Cartao_Credito
+(
 	id_cartao VARCHAR(16),
 	nivel_cartao VARCHAR(30) NOT NULL,
 	bandeira VARCHAR(30) NOT NULL,
-    ultimos_quatros_digitos VARCHAR(4) NOT NULL,
-    validade VARCHAR(10) NOT NULL,
-    cpf VARCHAR(11) NOT NULL,
+	ultimos_quatros_digitos VARCHAR(4) NOT NULL,
+	validade VARCHAR(10) NOT NULL,
+	cpf VARCHAR(11) NOT NULL,
 	PRIMARY KEY (id_cartao)
-); 
+);
 
-CREATE TABLE Loja_Fisica(
+CREATE TABLE Loja_Fisica
+(
 	cnpj VARCHAR(14) NOT NULL,
-    categoria VARCHAR(30) NULL,
-    id_endereco VARCHAR(60) NULL,
-    CONSTRAINT fk_endereco_loja_fisica FOREIGN KEY(id_endereco) REFERENCES Endereco(id_endereco),
+	categoria VARCHAR(30) NULL,
+	id_endereco VARCHAR(60) NULL,
+	CONSTRAINT fk_endereco_loja_fisica FOREIGN KEY(id_endereco) REFERENCES Endereco(id_endereco),
 	PRIMARY KEY (cnpj)
 );
 
-CREATE TABLE Faculdade(
+CREATE TABLE Faculdade
+(
 	cnpj VARCHAR(14) NOT NULL,
-    categoria VARCHAR(30) NULL,
-    id_endereco VARCHAR(60) NULL,
-    CONSTRAINT fk_endereco_faculdade FOREIGN KEY(id_endereco) REFERENCES Endereco(id_endereco),
+	categoria VARCHAR(30) NULL,
+	id_endereco VARCHAR(60) NULL,
+	CONSTRAINT fk_endereco_faculdade FOREIGN KEY(id_endereco) REFERENCES Endereco(id_endereco),
 	PRIMARY KEY(cnpj)
 );
 
-CREATE TABLE Compra_Fisica(
+CREATE TABLE Compra_Fisica
+(
 	id_transacao_fisica VARCHAR(60) NOT NULL,
 	cpf VARCHAR(11) NOT NULL,
 	cnpj VARCHAR(14) NOT NULL,
@@ -94,21 +103,23 @@ CREATE TABLE Compra_Fisica(
 	PRIMARY KEY (id_transacao_fisica)
 );
 
-CREATE TABLE Estuda(
+CREATE TABLE Estuda
+(
 	cnpj VARCHAR(14) NOT NULL,
-    cpf VARCHAR(11) NOT NULL, 
-    data_inicio VARCHAR(10) NULL,
-    bolsa BOOLEAN NULL,
-    valor_mensalidade FLOAT(60,2) NULL,
-    matricula VARCHAR(60) NULL,
-    PRIMARY KEY(cpf, cnpj),
+	cpf VARCHAR(11) NOT NULL,
+	data_inicio VARCHAR(10) NULL,
+	bolsa BOOLEAN NULL,
+	valor_mensalidade FLOAT(60,2) NULL,
+	matricula VARCHAR(60) NULL,
+	PRIMARY KEY(cpf, cnpj),
 	INDEX fk_compra_fisica_cpf_idx(cpf),
 	INDEX fk_compra_fisica_cnpj_idx(cnpj),
 	CONSTRAINT fk_estuda_cpf FOREIGN KEY(cpf) REFERENCES Pessoa(cpf),
 	CONSTRAINT fk_estuda_cnpj FOREIGN KEY(cnpj) REFERENCES Faculdade(cnpj)
 );
 
-CREATE TABLE Compra_Online(
+CREATE TABLE Compra_Online
+(
 	id_transacao_online VARCHAR(60) NOT NULL,
 	cpf VARCHAR(11) NOT NULL,
 	id_cartao VARCHAR(16),
@@ -124,7 +135,8 @@ CREATE TABLE Compra_Online(
 	PRIMARY KEY (id_transacao_online)
 );
 
-CREATE TABLE Renda_Media(
+CREATE TABLE Renda_Media
+(
 	cpf VARCHAR(11) NOT NULL,
 	CONSTRAINT fk_renda_media_cpf FOREIGN KEY(cpf) REFERENCES Pessoa(cpf),
 	categoria VARCHAR(60) NULL,
@@ -133,7 +145,8 @@ CREATE TABLE Renda_Media(
 	PRIMARY KEY(cpf)
 );
 
-CREATE TABLE Filiacao(
+CREATE TABLE Filiacao
+(
 	cpf_responsavel VARCHAR(11) NOT NULL,
 	cpf_dependente VARCHAR(11) NOT NULL,
 	CONSTRAINT fk_filiacao_cpf_dependente FOREIGN KEY(cpf_dependente) REFERENCES Pessoa(cpf),
@@ -141,7 +154,8 @@ CREATE TABLE Filiacao(
 	PRIMARY KEY(cpf_responsavel,cpf_dependente)
 );
 
-CREATE TABLE Indicacao(
+CREATE TABLE Indicacao
+(
 	cpf_indicado VARCHAR(11) NOT NULL,
 	cpf_indica VARCHAR(11) NOT NULL,
 	CONSTRAINT fk_indicacao_cpf_indicado FOREIGN KEY(cpf_indicado) REFERENCES Pessoa(cpf),
@@ -149,9 +163,10 @@ CREATE TABLE Indicacao(
 	PRIMARY KEY(cpf_indicado,cpf_indica)
 );
 
-CREATE TABLE Usa(
+CREATE TABLE Usa
+(
 	cpf_usuario VARCHAR(11) NOT NULL,
-    nome_aplicativo VARCHAR(60) NOT NULL,
+	nome_aplicativo VARCHAR(60) NOT NULL,
 	CONSTRAINT fk_usa_cpf_dependente FOREIGN KEY(cpf_usuario) REFERENCES Pessoa(cpf),
 	CONSTRAINT fk_usa_nome_aplicativo FOREIGN KEY(nome_aplicativo) REFERENCES Aplicativo(nome),
 	PRIMARY KEY(cpf_usuario,nome_aplicativo)
--- populateDatabase.sql
+++ populateDatabase.sql
@@ -1,56 +1,129 @@
 
 -- Populate Endereco
-INSERT INTO Endereco (id_endereco, numero, logradouro, cep, cidade, estado, bairro) VALUES('1', 22, 'Rua Quarenta e dois', "53080430", "Olinda", "Pernambuco", "Rio Doce");
-INSERT INTO Endereco (id_endereco, numero, logradouro, cep, cidade, estado, bairro) VALUES('2', 93, 'Rua Soledade e anjo', "52380430", "Recife", "Pernambuco", "Boa Viagem");
-INSERT INTO Endereco (id_endereco, numero, logradouro, cep, cidade, estado, bairro)  VALUES('3', 02, 'Rua Parnamirim', "52380420", "Recife", "Pernambuco", "Aflitos");
-INSERT INTO Endereco (id_endereco, numero, logradouro, cep, cidade, estado, bairro)  VALUES('4', 42, 'Avenida Brasil', "51872340", "Jaboatão", "Pernambuco", "Morro dos macacos");
-INSERT INTO Endereco (id_endereco, numero, logradouro, cep, cidade, estado, bairro)  VALUES('5', 77, 'Avenida Cais do Apolo', "52872231", "Recife", "Pernambuco", "Bairro do Recife");
-INSERT INTO Endereco (id_endereco, numero, logradouro, cep, cidade, estado, bairro)  VALUES('6', 00, 'Rua Professor Luiz Freire', "53032743", "Recife", "Pernambuco", "Cidade Universitária");
+INSERT INTO Endereco
+    (id_endereco, numero, logradouro, cep, cidade, estado, bairro)
+VALUES('1', 22, 'Rua Quarenta e dois', "53080430", "Olinda", "Pernambuco", "Rio Doce");
+INSERT INTO Endereco
+    (id_endereco, numero, logradouro, cep, cidade, estado, bairro)
+VALUES('2', 93, 'Rua Soledade e anjo', "52380430", "Recife", "Pernambuco", "Boa Viagem");
+INSERT INTO Endereco
+    (id_endereco, numero, logradouro, cep, cidade, estado, bairro)
+VALUES('3', 02, 'Rua Parnamirim', "52380420", "Recife", "Pernambuco", "Aflitos");
+INSERT INTO Endereco
+    (id_endereco, numero, logradouro, cep, cidade, estado, bairro)
+VALUES('4', 42, 'Avenida Brasil', "51872340", "Jaboatão", "Pernambuco", "Morro dos macacos");
+INSERT INTO Endereco
+    (id_endereco, numero, logradouro, cep, cidade, estado, bairro)
+VALUES('5', 77, 'Avenida Cais do Apolo', "52872231", "Recife", "Pernambuco", "Bairro do Recife");
+INSERT INTO Endereco
+    (id_endereco, numero, logradouro, cep, cidade, estado, bairro)
+VALUES('6', 00, 'Rua Professor Luiz Freire', "53032743", "Recife", "Pernambuco", "Cidade Universitária");
 
 -- Populate Pessoa
-INSERT INTO Pessoa (cpf, nome, data_de_nascimento, id_endereco, genero, nome_mae) VALUES ("10097142441", "Otacilio Maia Fakersom", "14/11/1996", '1', 'masculino', 'Liliam Freire');
-INSERT INTO Pessoa (cpf, nome, data_de_nascimento, id_endereco, genero, nome_mae) VALUES ("43157231547", "Maria Mentirosa", "01/03/2002", '2', 'feminino', 'Laura Mentirosa');
-INSERT INTO Pessoa (cpf, nome, data_de_nascimento, id_endereco, genero, nome_mae) VALUES ("12871289732", "Laura Mentirosa", "11/05/1940", '2', 'feminino', 'Paula Mentirosa');
+INSERT INTO Pessoa
+    (cpf, nome, data_de_nascimento, id_endereco, genero, nome_mae)
+VALUES
+    ("10097142441", "Otacilio Maia Fakersom", "14/11/1996", '1', 'masculino', 'Liliam Freire');
+INSERT INTO Pessoa
+    (cpf, nome, data_de_nascimento, id_endereco, genero, nome_mae)
+VALUES
+    ("43157231547", "Maria Mentirosa", "01/03/2002", '2', 'feminino', 'Laura Mentirosa');
+INSERT INTO Pessoa
+    (cpf, nome, data_de_nascimento, id_endereco, genero, nome_mae)
+VALUES
+    ("12871289732", "Laura Mentirosa", "11/05/1940", '2', 'feminino', 'Paula Mentirosa');
 
 -- Populate Aplicativo
-INSERT INTO Aplicativo (nome, categoria, faz_transacao) VALUES ("Tinder", "Casual/Paquera", 1);
-INSERT INTO Aplicativo (nome, categoria, faz_transacao) VALUES ("Uber", "Mobilidade", 1);
-INSERT INTO Aplicativo (nome, categoria, faz_transacao) VALUES ("Guia de Bolso", "Finanças", 0);
+INSERT INTO Aplicativo
+    (nome, categoria, faz_transacao)
+VALUES
+    ("Tinder", "Casual/Paquera", 1);
+INSERT INTO Aplicativo
+    (nome, categoria, faz_transacao)
+VALUES
+    ("Uber", "Mobilidade", 1);
+INSERT INTO Aplicativo
+    (nome, categoria, faz_transacao)
+VALUES
+    ("Guia de Bolso", "Finanças", 0);
 
 -- Populate Telefone
-INSERT INTO Telefone (numero, cpf) VALUES (34316829, "43157231547");
-INSERT INTO Telefone (numero, cpf) VALUES (999724356, "43157231547");
-INSERT INTO Telefone (numero, cpf) VALUES (999597078, "10097142441");
+INSERT INTO Telefone
+    (numero, cpf)
+VALUES
+    (34316829, "43157231547");
+INSERT INTO Telefone
+    (numero, cpf)
+VALUES
+    (999724356, "43157231547");
+INSERT INTO Telefone
+    (numero, cpf)
+VALUES
+    (999597078, "10097142441");
 
 -- Populate Cartão de Crédito
-INSERT INTO Cartao_Credito (id_cartao, nivel_cartao, bandeira, ultimos_quatros_digitos, validade, cpf) VALUES ("5258134325674997", "Platinum", "Mastercard", "0021", "02/29", "43157231547");
-INSERT INTO Cartao_Credito (id_cartao, nivel_cartao, bandeira, ultimos_quatros_digitos, validade, cpf) VALUES ("4556780233046280", "Nanquim", "Visa", "5123", "01/23", "10097142441");
+INSERT INTO Cartao_Credito
+    (id_cartao, nivel_cartao, bandeira, ultimos_quatros_digitos, validade, cpf)
+VALUES
+    ("5258134325674997", "Platinum", "Mastercard", "0021", "02/29", "43157231547");
+INSERT INTO Cartao_Credito
+    (id_cartao, nivel_cartao, bandeira, ultimos_quatros_digitos, validade, cpf)
+VALUES
+    ("4556780233046280", "Nanquim", "Visa", "5123", "01/23", "10097142441");
 -- Person out of Pessoa's base
-INSERT INTO Cartao_Credito (id_cartao, nivel_cartao, bandeira, ultimos_quatros_digitos, validade, cpf) VALUES ("6011620456406247", "Top Blaster", "Discover", "3126", "09/32", "10394152453");
+INSERT INTO Cartao_Credito
+    (id_cartao, nivel_cartao, bandeira, ultimos_quatros_digitos, validade, cpf)
+VALUES
+    ("6011620456406247", "Top Blaster", "Discover", "3126", "09/32", "10394152453");
 
 -- Populate Loja Fisica
-INSERT INTO Loja_Fisica (cnpj, categoria, id_endereco) VALUES ("92863080000127", "Atacado", '4');
-INSERT INTO Loja_Fisica (cnpj, categoria, id_endereco) VALUES ("34160873000183", "Sorveteria", NULL);
+INSERT INTO Loja_Fisica
+    (cnpj, categoria, id_endereco)
+VALUES
+    ("92863080000127", "Atacado", '4');
+INSERT INTO Loja_Fisica
+    (cnpj, categoria, id_endereco)
+VALUES
+    ("34160873000183", "Sorveteria", NULL);
 
 -- Populate Faculdade
-INSERT INTO Faculdade (cnpj, categoria, id_endereco) VALUES ("96367632000167", "Privada", '5');
-INSERT INTO Faculdade (cnpj, categoria, id_endereco) VALUES ("98247507000149", "Publica", '6');
+INSERT INTO Faculdade
+    (cnpj, categoria, id_endereco)
+VALUES
+    ("96367632000167", "Privada", '5');
+INSERT INTO Faculdade
+    (cnpj, categoria, id_endereco)
+VALUES
+    ("98247507000149", "Publica", '6');
 
 -- Populate compra_fisica
-INSERT INTO Compra_Fisica(id_transacao_fisica, cpf, cnpj, id_cartao, parcelamento, tipo_de_pagamento, data_compra, valor) VALUES("1", '43157231547', "92863080000127", "5258134325674997", NULL, "Credito a vista", "01/10/2019", 4099.52); 
+INSERT INTO Compra_Fisica
+    (id_transacao_fisica, cpf, cnpj, id_cartao, parcelamento, tipo_de_pagamento, data_compra, valor)
+VALUES("1", '43157231547', "92863080000127", "5258134325674997", NULL, "Credito a vista", "01/10/2019", 4099.52);
 
 -- Populate Estuda
-INSERT INTO Estuda (cnpj, cpf, data_inicio, bolsa, valor_mensalidade, matricula) VALUES ("96367632000167", "43157231547", "01/03/2018", 0, 2300.00, "2018101021");
+INSERT INTO Estuda
+    (cnpj, cpf, data_inicio, bolsa, valor_mensalidade, matricula)
+VALUES
+    ("96367632000167", "43157231547", "01/03/2018", 0, 2300.00, "2018101021");
 
 -- Populate Renda Mensal
-INSERT INTO Renda_Media(cpf, categoria, data_recebimento, valor) VALUES("10097142441", "Renda fixa", "Dia 30", 1400.00);
+INSERT INTO Renda_Media
+    (cpf, categoria, data_recebimento, valor)
+VALUES("10097142441", "Renda fixa", "Dia 30", 1400.00);
 
 -- Populate Filicacao
-INSERT INTO Filiacao(cpf_responsavel, cpf_dependente) VALUES("12871289732", "43157231547");
+INSERT INTO Filiacao
+    (cpf_responsavel, cpf_dependente)
+VALUES("12871289732", "43157231547");
 
 -- Populate Indicação
-INSERT INTO Indicacao(cpf_indicado, cpf_indica) VALUES("10097142441", "43157231547");
+INSERT INTO Indicacao
+    (cpf_indicado, cpf_indica)
+VALUES("10097142441", "43157231547");
 
 -- Populate Usa
-INSERT INTO Usa(cpf_usuario, nome_aplicativo) VALUES("10097142441", "Guia de Bolso");
+INSERT INTO Usa
+    (cpf_usuario, nome_aplicativo)
+VALUES("10097142441", "Guia de Bolso");
 
