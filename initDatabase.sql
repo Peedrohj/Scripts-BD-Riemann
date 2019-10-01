4514b94053d6cbf69646d6a3ddd7ade9a7e6dbd3
OtacilioN <osmn@cesar.org.br>
2019-10-01 19:40:45 -0300

    Fix InitDatabase issues

--- initDatabase.sql
+++ initDatabase.sql
@@ -1,5 +1,6 @@
 -- drop database Riemann
 -- Create project database
+DROP DATABASE IF EXISTS Riemann;
 CREATE DATABASE IF NOT EXISTS Riemann;
 USE Riemann;
 
@@ -20,14 +21,14 @@ DROP TABLE IF EXISTS Loja_Fisica;
 CREATE TABLE Aplicativo (
 	nome VARCHAR(60) NOT NULL,
 	categoria VARCHAR(45) NULL,
-	faz_transacao INT NULL,
+	faz_transacao BIT NULL DEFAULT 0,
 	PRIMARY KEY (nome)
 );
 
 CREATE TABLE Endereco(
 	id_endereco VARCHAR(60) NOT NULL, 
 	numero INT(5) NULL,
-    logradouro VARCHAR(16) NULL,
+    logradouro VARCHAR(90) NULL,
     cep VARCHAR(8) NULL,
     cidade VARCHAR(95) NULL,
     estado VARCHAR(90) NULL,
@@ -36,7 +37,7 @@ CREATE TABLE Endereco(
 );
 
 CREATE TABLE Pessoa(
-	cpf INT(11) NOT NULL,
+	cpf VARCHAR(11) NOT NULL,
 	nome VARCHAR(90) NULL,
 	data_de_nascimento VARCHAR(10) NULL,
 	id_endereco VARCHAR(60) NULL, 
@@ -48,15 +49,15 @@ CREATE TABLE Pessoa(
 
 CREATE TABLE Telefone(
 	numero VARCHAR(13) NULL,
-    cpf INT(11) NOT NULL, 
+    cpf VARCHAR(11) NOT NULL, 
 	CONSTRAINT fk_pessoa_faculdade FOREIGN KEY (cpf) REFERENCES Pessoa (cpf)
 );
 
 CREATE TABLE Cartao_Credito(
-	id_cartao INT(16),
+	id_cartao VARCHAR(16),
 	nivel_cartao VARCHAR(30) NOT NULL,
 	bandeira VARCHAR(30) NOT NULL,
-    ultimos_quatros_digitos INT(4) NOT NULL,
+    ultimos_quatros_digitos VARCHAR(4) NOT NULL,
     validade VARCHAR(10) NOT NULL,
     cpf VARCHAR(11) NOT NULL,
 	PRIMARY KEY (id_cartao)
@@ -80,9 +81,9 @@ CREATE TABLE Faculdade(
 
 CREATE TABLE Compra_Fisica(
 	id_transacao_fisica VARCHAR(60) NOT NULL,
-	cpf INT(11) NOT NULL,
+	cpf VARCHAR(11) NOT NULL,
 	cnpj VARCHAR(14) NOT NULL,
-	id_cartao INT(16),
+	id_cartao VARCHAR(16),
 	CONSTRAINT fk_compra_fisica_cpf FOREIGN KEY(cpf) REFERENCES Pessoa(cpf),
 	CONSTRAINT fk_compra_fisica_cnpj FOREIGN KEY(cnpj) REFERENCES Loja_Fisica(cnpj),
 	CONSTRAINT fk_compra_fisica_id_cartao FOREIGN KEY(id_cartao) REFERENCES Cartao_Credito(id_cartao),
@@ -95,7 +96,7 @@ CREATE TABLE Compra_Fisica(
 
 CREATE TABLE Estuda(
 	cnpj VARCHAR(14) NOT NULL,
-    cpf INT(11) NOT NULL, 
+    cpf VARCHAR(11) NOT NULL, 
     data_inicio VARCHAR(10) NULL,
     bolsa BOOLEAN NULL,
     valor_mensalidade FLOAT(60,2) NULL,
@@ -104,13 +105,13 @@ CREATE TABLE Estuda(
 	INDEX fk_compra_fisica_cpf_idx(cpf),
 	INDEX fk_compra_fisica_cnpj_idx(cnpj),
 	CONSTRAINT fk_estuda_cpf FOREIGN KEY(cpf) REFERENCES Pessoa(cpf),
-	CONSTRAINT fk_estuda_cnpj FOREIGN KEY(cnpj) REFERENCES Loja_Fisica(cnpj)
+	CONSTRAINT fk_estuda_cnpj FOREIGN KEY(cnpj) REFERENCES Faculdade(cnpj)
 );
 
 CREATE TABLE Compra_Online(
 	id_transacao_online VARCHAR(60) NOT NULL,
-	cpf INT(11) NOT NULL,
-	id_cartao INT(16),
+	cpf VARCHAR(11) NOT NULL,
+	id_cartao VARCHAR(16),
 	nome_aplicativo VARCHAR(60) NOT NULL,
 	CONSTRAINT fk_compra_online_cpf FOREIGN KEY(cpf) REFERENCES Pessoa(cpf),
 	CONSTRAINT fk_compra_online_nome_aplicativo FOREIGN KEY(nome_aplicativo) REFERENCES Aplicativo(nome),
@@ -124,31 +125,32 @@ CREATE TABLE Compra_Online(
 );
 
 CREATE TABLE Renda_Media(
-	cpf INT(11) NOT NULL,
+	cpf VARCHAR(11) NOT NULL,
 	CONSTRAINT fk_renda_media_cpf FOREIGN KEY(cpf) REFERENCES Pessoa(cpf),
 	categoria VARCHAR(60) NULL,
 	data_recebimento VARCHAR(10) NULL,
+	valor FLOAT(60, 2) NULL,
 	PRIMARY KEY(cpf)
 );
 
 CREATE TABLE Filiacao(
-	cpf_responsavel INT(11) NOT NULL,
-	cpf_dependente INT(11) NOT NULL,
+	cpf_responsavel VARCHAR(11) NOT NULL,
+	cpf_dependente VARCHAR(11) NOT NULL,
 	CONSTRAINT fk_filiacao_cpf_dependente FOREIGN KEY(cpf_dependente) REFERENCES Pessoa(cpf),
 	CONSTRAINT fk_filiacao_cpf_responsavel FOREIGN KEY(cpf_responsavel) REFERENCES Pessoa(cpf),
 	PRIMARY KEY(cpf_responsavel,cpf_dependente)
 );
 
 CREATE TABLE Indicacao(
-	cpf_responsavel INT(11) NOT NULL,
-	cpf_dependente INT(11) NOT NULL,
-	CONSTRAINT fk_indicacao_cpf_dependente FOREIGN KEY(cpf_dependente) REFERENCES Pessoa(cpf),
-	CONSTRAINT fk_indicacao_cpf_responsavel FOREIGN KEY(cpf_responsavel) REFERENCES Pessoa(cpf),
-	PRIMARY KEY(cpf_responsavel,cpf_dependente)
+	cpf_indicado VARCHAR(11) NOT NULL,
+	cpf_indica VARCHAR(11) NOT NULL,
+	CONSTRAINT fk_indicacao_cpf_indicado FOREIGN KEY(cpf_indicado) REFERENCES Pessoa(cpf),
+	CONSTRAINT fk_indicacao_cpf_indica FOREIGN KEY(cpf_indica) REFERENCES Pessoa(cpf),
+	PRIMARY KEY(cpf_indicado,cpf_indica)
 );
 
 CREATE TABLE Usa(
-	cpf_usuario INT(11) NOT NULL,
+	cpf_usuario VARCHAR(11) NOT NULL,
     nome_aplicativo VARCHAR(60) NOT NULL,
 	CONSTRAINT fk_usa_cpf_dependente FOREIGN KEY(cpf_usuario) REFERENCES Pessoa(cpf),
 	CONSTRAINT fk_usa_nome_aplicativo FOREIGN KEY(nome_aplicativo) REFERENCES Aplicativo(nome),
